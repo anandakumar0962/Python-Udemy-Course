05a435197b039840aad283b0acebdd8958804a97
Anandakumar A <111185338+anandakumar0962@users.noreply.github.com>
2023-10-27 08:30:23 +0530

    Add files via upload

---
+++ Projects/Day 21-30/Day 23/Turtle crossing game/main.py
@@ -0,0 +1,37 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+screen.listen()
+screen.onkeypress(player.go_up,'Up')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.generate_car()
+    car_manager.move_cars()
+
+    #Detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect whether the player reached the finish line
+    if player.is_level_finish():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.update_level()
+
+
+screen.exitonclick()
